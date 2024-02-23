8b56c94067211bb27ebb4bad05fea5f69d023c00
Dhruv <dhruvrajput1904200@gmail.com>
2024-02-23 19:22:22 +0530

    fix

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let box = document.querySelectorAll(".box");
-let Reset = document.querySelector("#Reset");
-
-let turn0 = true;
-
+const box = document.querySelectorAll(".box");
+const resetbutt = document.querySelector("#resetButt");
+const msgcont = document.querySelector(".msgCont");
+const msg = document.querySelector("#msg");
+const newButt = document.querySelector("#new-button");
+let turnO = true; //player x & player o
 const win = [
   [0, 1, 2],
   [3, 4, 5],
@@ -14,31 +15,69 @@ const win = [
   [2, 4, 6],
 ];
 
-box.forEach((element) => {
-  element.addEventListener("click", () => {
-    console.log("box");
-    if (turn0) {
-      element.innerText = "O";
-      turn0 = false;
+//*check whoses turn or run function check winner
+box.forEach((boxes) => {
+  boxes.addEventListener("click", () => {
+    // console.log("box was click");
+    if (turnO) {
+      boxes.innerText = "O";
+      turnO = false;
     } else {
-      element.innerText = "X";
-      turn0 = true;
+      boxes.innerText = "X";
+      turnO = true;
     }
-    element.disabled = true;
+    boxes.disabled = true;
 
-    winner();
+    checkWinner();
   });
 });
 
-const winner = () => {
-  for (const pattern of win) {
-    let pot1 = box[pattern[0]].innerText;
-    let pot2 = box[pattern[1]].innerText;
-    let pot3 = box[pattern[2]].innerText;
-    if (pot1 != "" && pot2 != "" && pot3 != "") {
-      if (pot1 === pot2 && pot2 === pot3) {
-        console.log("winner " + pot1);
+//*checkWinner
+const checkWinner = () => {
+  win.forEach((winner) => {
+    /*console.log(winner[0], winner[1], winner[2]);
+      console.log(
+        box[winner[1]].innerText,
+        box[winner[2]].innerText
+        ); 
+    */
+    let post1 = box[winner[0]].innerText;
+    let post2 = box[winner[1]].innerText;
+    let post3 = box[winner[2]].innerText;
+    if (post1 != "" && post2 != "" && post3 != "") {
+      if (post1 == post2 && post2 == post3) {
+        // console.log("winner " + post1);
+        showwinner(post1);
       }
     }
+  });
+};
+//*show winner
+const showwinner = (post1) => {
+  msg.innerText = `winner is ${post1}`;
+  msgcont.classList.remove("hide");
+  disableButt();
+};
+//*Rest game
+const restGame = () => {
+  turnO = true;
+  enableButt();
+  msgcont.classList.add("hide");
+};
+resetbutt.addEventListener("click", restGame);
+
+//*disble buttons after game is complete
+const disableButt = () => {
+  for (const disable of box) {
+    disable.disabled = true;
   }
 };
+
+const enableButt = () => {
+  for (const boxes of box) {
+    boxes.disabled = false;
+    boxes.innerText = "";
+  }
+};
+//*new game butt
+newButt.addEventListener("click", restGame);
